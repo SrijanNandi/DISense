bb00b641cb15c47de64156ddd8c6234114a4c1d2
Devel <sales@infrassist.com>
2017-11-01 07:15:08 +0000

    AISense Changes

--- src/usr/local/www/FauxAPI/pkg_install_fauxapi.sh
+++ src/usr/local/www/FauxAPI/pkg_install_fauxapi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pkg_fauxapi=`pkg info pfSense-pkg-FauxAPI-1.1 | head -n 1`
+pkg_fauxapi=`pkg info pfSense-pkg-FauxAPI-1.1 | head -n 1	`
 if [ "$pkg_fauxapi" = "pfSense-pkg-FauxAPI-1.1" 2>&1 >/dev/null ]; then
         echo "FauxAPI is already installed"
 else
